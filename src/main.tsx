6ac0b62ec11b615792f21e0c1520db024b395b56
Seoin <seoin2269@naver.com>
2025-01-05 11:40:26 +0900

    feat: diff 알고리즘 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,20 @@
-type Props = { [key: string]: any };
+type RecordType = Record<string, any>;
 
 interface VirtualNode {
   type: string;
-  props: Props;
-  children: VirtualNode[];
+  props: RecordType;
+  children: (VirtualNode | string)[];
 }
 
-function h(type: string, props: Props | null, ...children: any[]) {
-  if (children.length === 0) {
-    return {
-      type,
-      props,
-      children: [],
-    };
-  }
-
-  const flatChildren = children.flat().filter((child) => child != null);
-
+function h(type: string, props: RecordType | null, ...children: any[]): VirtualNode {
   return {
     type,
     props: props || {},
-    children: flatChildren,
+    children: children.flat().filter((child) => child != null),
   };
 }
 
-function createElement(node: VirtualNode): Node {
+function createElement(node: VirtualNode | string): Node {
   if (typeof node === 'string') {
     return document.createTextNode(node);
   }
@@ -40,12 +30,53 @@ function createElement(node: VirtualNode): Node {
   return $element;
 }
 
-const state = [
-  { id: 1, completed: false, content: 'todo list item 1' },
-  { id: 2, completed: true, content: 'todo list item 2' },
-];
+function updateElement(parent: Node, oldNode: VirtualNode | string, newNode: VirtualNode | string, index = 0) {
+  const childNode = parent.childNodes[index];
 
-const realDom = createElement(
+  if (!newNode && oldNode) {
+    if (childNode) parent.removeChild(childNode);
+    return;
+  }
+
+  if (newNode && !oldNode) {
+    parent.appendChild(createElement(newNode));
+    return;
+  }
+
+  if (typeof newNode === 'string' && typeof oldNode === 'string') {
+    if (newNode !== oldNode) {
+      if (childNode) parent.replaceChild(createElement(newNode), childNode);
+    }
+    return;
+  }
+
+  if (typeof newNode !== 'string' && typeof oldNode !== 'string' && newNode.type !== oldNode.type) {
+    if (childNode) parent.replaceChild(createElement(newNode), childNode);
+    return;
+  }
+
+  if (typeof oldNode !== 'string' && typeof newNode !== 'string') {
+    if (childNode instanceof HTMLElement) {
+      updateAttributes(childNode, newNode.props, oldNode.props);
+    }
+
+    const maxLength = Math.max(newNode.children.length, oldNode.children.length);
+    for (let i = 0; i < maxLength; i++) {
+      updateElement(childNode!, oldNode.children[i], newNode.children[i], i);
+    }
+  }
+
+  function updateAttributes(target: HTMLElement, newProps: RecordType, oldProps: RecordType) {
+    for (const [attr, value] of Object.entries(newProps)) {
+      if (oldProps[attr] !== value) target.setAttribute(attr, value);
+    }
+    for (const attr of Object.keys(oldProps)) {
+      if (!(attr in newProps)) target.removeAttribute(attr);
+    }
+  }
+}
+
+const render = (state: RecordType[]) => (
   <div id="app">
     <ul>
       {state.map(({ completed, content }) => (
@@ -60,7 +91,26 @@ const realDom = createElement(
       <input type="text" />
       <button type="submit">추가</button>
     </form>
-  </div>,
+  </div>
 );
 
-document.body.appendChild(realDom);
+const oldState = [
+  { id: 1, completed: false, content: 'todo list item 1' },
+  { id: 2, completed: true, content: 'todo list item 2' },
+];
+
+const newState = [
+  { id: 1, completed: true, content: 'todo list item 1 updated' },
+  { id: 2, completed: true, content: 'todo list item 2' },
+  { id: 3, completed: false, content: 'todo list item 3' },
+];
+
+const oldNode = render(oldState);
+const newNode = render(newState);
+
+const $root = document.createElement('div');
+document.body.appendChild($root);
+
+$root.appendChild(createElement(oldNode));
+
+setTimeout(() => updateElement($root, oldNode, newNode), 1000);
