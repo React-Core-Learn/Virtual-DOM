fed4b87333e7e53f45bc48ffc0c600c5eef138e9
Seoin <seoin2269@naver.com>
2025-01-05 11:35:30 +0900

    chore: 불필요한 코드 제거

--- src/main.tsx
+++ src/main.tsx
@@ -55,13 +55,6 @@ const realDom = createElement(
           <button class="remove">삭제</button>
         </li>
       ))}
-      {state.map(({ completed, content }) => (
-        <li class={completed ? 'completed' : null}>
-          <input type="checkbox" class="toggle" checked={completed} />
-          {content}
-          <button class="remove">삭제</button>
-        </li>
-      ))}
     </ul>
     <form>
       <input type="text" />
